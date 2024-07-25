fb32e9c93452a9ae15f13a0f783d43361db7c0c2
unknown <toandv.icv@gmail.com>
2024-07-25 22:10:20 +0700

    add map view to UI

---
+++ src/define.py
@@ -0,0 +1,4 @@
+EXAMPLE_IMG_PATH    = 'logs/images/processing/240725_123456.jpg'
+EXAMPLE_MAP_PATH    = 'logs/images/processing/map_240724_123456.jpg'
+NULL_IMG_PATH       = 'assets/null.png'
+MAIN_UI_PATH        = 'UI/main.ui'
---
+++ src/main.py
@@ -0,0 +1,52 @@
+'''
+Usage: python -m src.main
+'''
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QByteArray
+from .define import *
+
+
+class MAIN(QMainWindow):
+    IMG_W   = 525
+    IMG_H   = 290
+    MAP_W   = 180
+    MAP_H   = 290
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        loadUi(MAIN_UI_PATH, self)
+        self.connectSignalsSlots()
+
+    def connectSignalsSlots(self):
+        self.viewSnapshoot()
+        self.viewMap()
+
+    def viewSnapshoot(self):
+        pixmap = QPixmap(EXAMPLE_IMG_PATH)
+        pixmap = pixmap.scaled(MAIN.IMG_W, MAIN.IMG_H, Qt.KeepAspectRatio)
+        self.lbl_display.setPixmap(pixmap)
+        self.lbl_display.resize(MAIN.IMG_W, MAIN.IMG_H)
+        self.show()
+
+    def viewMap(self):
+        pixmap = QPixmap(EXAMPLE_MAP_PATH)
+        pixmap = pixmap.scaled(MAIN.MAP_W, MAIN.MAP_H, Qt.KeepAspectRatio)
+        self.lbl_map.setPixmap(pixmap)
+        self.lbl_map.resize(MAIN.MAP_W, MAIN.MAP_H)
+        self.show()
+
+    def viewNone(self):
+        pixmap = QPixmap(NULL_IMG_PATH)
+        pixmap = pixmap.scaled(MAIN.W_DISP, MAIN.H_DISP, Qt.KeepAspectRatio)
+        self.lbl_display.setPixmap(pixmap)
+        self.lbl_display.resize(MAIN.W_DISP, MAIN.H_DISP)
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MAIN()
+    win.show()
+    sys.exit(app.exec())
